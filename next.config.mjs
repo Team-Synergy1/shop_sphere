04a057bf6248e82fbfd70f7343dc77de9df476be
Ayub Islam <ayubislam863@gmail.com>
2025-04-30 17:25:03 +0600

    [Modify]:the add product and category page

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	// Ignore certificates folder to prevent unnecessary Fast Refresh
+	
 	watchOptions: {
 		ignored: ["**/certificates/**"],
 	},
